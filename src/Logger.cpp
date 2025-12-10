4e945c17dfbea532dac65ce44e100844ae74aa26
Anatoliy <and1life@yandex.ru>
2025-12-10 15:09:42 +0300

    feat(logger): added implementation debug(), info(), warning(), error(), critical()

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -9,3 +9,28 @@ void Logger::setLevel(Level level) noexcept
     std::lock_guard<std::mutex> lock(mutex);
     currentLevel = level;
 }
+
+void Logger::debug(const std::string &message)
+{
+    log(Level::DEBUG, message);
+}
+
+void Logger::info(const std::string &message)
+{
+    log(Level::INFO, message);
+}
+
+void Logger::warning(const std::string &message)
+{
+    log(Level::WARNING, message);
+}
+
+void Logger::error(const std::string &message)
+{
+    log(Level::ERROR, message);
+}
+
+void Logger::critical(const std::string &message)
+{
+    log(Level::CRITICAL, message);
+}
