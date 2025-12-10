0553c17e63ee323a1a13711a6a3eba8df5361cb3
Anatoliy <and1life@yandex.ru>
2025-12-10 14:28:16 +0300

    feat(logger): added basic Logger interface with levels and sinks

---
+++ include/logger/LogSink.hpp
@@ -0,0 +1,9 @@
+#include <string>
+
+class LogSink
+{
+public:
+
+    virtual ~LogSink() = default;
+    virtual void write(const std::string& message) = 0;
+};
---
+++ include/logger/Logger.hpp
@@ -0,0 +1,29 @@
+#include "LogSink.hpp"
+#include <mutex>
+#include <memory>
+#include <vector>
+
+class Logger
+{
+public:
+
+    enum class Level { DEBUG, INFO, WARNING, ERROR, CRITICAL };
+    
+    void setLevel(Level level);
+    void addSink(std::shared_ptr<LogSink> sink);
+    void removeSink(std::shared_ptr<LogSink> sink);
+
+    void debug(const std::string& message);
+    void indo(const std::string& message);
+    void warning(const std::string& message);
+    void error(const std::string& message);
+    void critical(const std::string& message);
+
+private:
+    Level currentLevel = Level::DEBUG;
+    std::vector<std::shared_ptr<LogSink>> sinks;
+    std::mutex mutex;
+
+    void log(Level level, const std::string& message);
+    std::string formatMessage(Level level, const std::string& message);
+};
